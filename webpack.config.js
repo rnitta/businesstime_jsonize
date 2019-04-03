7f7d281fd0558bd5fac843ab1c7de0a278df289f
rnitta <attinyes@gmail.com>
2019-04-03 15:08:10 +0900

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'docs'),
   },
   plugins: [new VueLoaderPlugin()],
   module: {
